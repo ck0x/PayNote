934422be624b13f55bc6f0e88996db2f5dcb92b0
TrissyG <tmon261@aucklanduni.ac.nz>
2025-10-27 00:58:42 +1300

    feat: update background effect to squares (v2)

--- frontend/paynote/src/components/animation/framer/background-effect.tsx
+++ frontend/paynote/src/components/animation/framer/background-effect.tsx
@@ -2,13 +2,13 @@
 
 import { useEffect, useState } from "react";
 
-interface AnimatedLine {
+interface AnimatedBlock {
   id: number;
   yPosition: number;
   duration: number;
   delay: number;
-  height: number;
-  opacity: number;
+  size: number;
+  color: string;
 }
 
 export default function HeroBlockAnimation() {
@@ -21,39 +21,47 @@ export default function HeroBlockAnimation() {
     return () => window.removeEventListener("resize", checkMobile);
   }, []);
 
-  const animatedLines: AnimatedLine[] = isMobile
+  const categoryColors = [
+    "#FDE68A", // Personal - Yellow
+    "#BFDBFE", // Operations - Blue
+    "#FCA5A5", // Payroll - Red
+    "#C4B5FD", // Research & Development - Purple
+    "#6EE7B7", // Customer - Green
+  ];
+
+  const animatedBlocks: AnimatedBlock[] = isMobile
     ? [
         {
           id: 1,
           yPosition: 15,
           duration: 12,
           delay: 0,
-          height: 3,
-          opacity: 0.25,
+          size: 4,
+          color: categoryColors[0],
         },
         {
           id: 2,
           yPosition: 35,
           duration: 14,
           delay: 2,
-          height: 4,
-          opacity: 0.2,
+          size: 4,
+          color: categoryColors[1],
         },
         {
           id: 3,
           yPosition: 55,
           duration: 13,
           delay: 4,
-          height: 2.5,
-          opacity: 0.3,
+          size: 4,
+          color: categoryColors[2],
         },
         {
           id: 4,
           yPosition: 75,
           duration: 15,
           delay: 6,
-          height: 3.5,
-          opacity: 0.22,
+          size: 4,
+          color: categoryColors[3],
         },
       ]
     : [
@@ -62,60 +70,58 @@ export default function HeroBlockAnimation() {
           yPosition: 10,
           duration: 12,
           delay: 0,
-          height: 3,
-          opacity: 0.28,
+          size: 4,
+          color: categoryColors[0],
         },
         {
           id: 2,
           yPosition: 22,
           duration: 14,
           delay: 1.5,
-          height: 4,
-          opacity: 0.22,
+          size: 4,
+          color: categoryColors[1],
         },
         {
           id: 3,
           yPosition: 35,
           duration: 13,
           delay: 3,
-          height: 2.5,
-          opacity: 0.3,
+          size: 4,
+          color: categoryColors[2],
         },
         {
           id: 4,
           yPosition: 48,
           duration: 15,
           delay: 4.5,
-          height: 3.5,
-          opacity: 0.25,
+          size: 4,
+          color: categoryColors[3],
         },
         {
           id: 5,
           yPosition: 62,
           duration: 16,
           delay: 6,
-          height: 3,
-          opacity: 0.2,
+          size: 4,
+          color: categoryColors[4],
         },
         {
           id: 6,
           yPosition: 78,
           duration: 14.5,
           delay: 7.5,
-          height: 4,
-          opacity: 0.26,
+          size: 4,
+          color: categoryColors[0],
         },
         {
           id: 7,
           yPosition: 88,
           duration: 13.5,
           delay: 9,
-          height: 2.8,
-          opacity: 0.24,
+          size: 4,
+          color: categoryColors[1],
         },
       ];
-
-  const primaryColor = "#00bf63";
 
   return (
     <div className="absolute inset-0 w-full h-full overflow-hidden">
@@ -124,41 +130,60 @@ export default function HeroBlockAnimation() {
         className="absolute w-full h-full"
         preserveAspectRatio="none"
       >
-        {animatedLines.map((line) => (
-          <g key={line.id}>
+        {animatedBlocks.map((block) => (
+          <g key={block.id}>
             <rect
-              x="-20"
-              y={line.yPosition}
-              width="15"
-              height={line.height}
-              fill={primaryColor}
-              opacity={line.opacity}
+              x="-10"
+              y={block.yPosition}
+              width={block.size}
+              height={block.size}
+              fill={block.color}
+              opacity="0.35"
               rx="0.5"
             >
               <animate
                 attributeName="x"
-                from="-20"
+                from="-10"
                 to="105"
-                dur={`${line.duration}s`}
-                begin={`${line.delay}s`}
+                dur={`${block.duration}s`}
+                begin={`${block.delay}s`}
+                repeatCount="indefinite"
+              />
+            </rect>
+
+            <rect
+              x="-10"
+              y={block.yPosition}
+              width={block.size}
+              height={block.size}
+              fill={categoryColors[(block.id + 2) % categoryColors.length]}
+              opacity="0.25"
+              rx="0.5"
+            >
+              <animate
+                attributeName="x"
+                from="-10"
+                to="105"
+                dur={`${block.duration}s`}
+                begin={`${block.delay + block.duration * 0.4}s`}
                 repeatCount="indefinite"
               />
             </rect>
             <rect
-              x="-20"
-              y={line.yPosition}
-              width="8"
-              height={line.height}
-              fill={primaryColor}
-              opacity={line.opacity * 0.5}
+              x="-10"
+              y={block.yPosition}
+              width={block.size}
+              height={block.size}
+              fill={categoryColors[(block.id + 4) % categoryColors.length]}
+              opacity="0.2"
               rx="0.5"
             >
               <animate
                 attributeName="x"
-                from="-20"
+                from="-10"
                 to="105"
-                dur={`${line.duration}s`}
-                begin={`${line.delay + line.duration * 0.4}s`}
+                dur={`${block.duration}s`}
+                begin={`${block.delay + block.duration * 0.7}s`}
                 repeatCount="indefinite"
               />
             </rect>
